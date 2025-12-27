dce4a35c0f032ed789b5db21c46b4b6b3215ab18
maheswarsarma <31466024+maheswarsarma@users.noreply.github.com>
2025-12-27 20:14:06 +0530

    Refactor AppTest class and import assertions

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,9 +1,9 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest {
+class AppTest {
 
     @Test
     void testAdd() {
