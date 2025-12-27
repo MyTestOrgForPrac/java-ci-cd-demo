689db826b67640fff3d48f08b807e3e7401afef8
maheswarsarma <31466024+maheswarsarma@users.noreply.github.com>
2025-12-27 20:13:38 +0530

    Update main method to print sum of two numbers

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,6 +7,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello CI/CD with Jenkins");
+        System.out.println("Sum = " + add(2, 3));
     }
 }
